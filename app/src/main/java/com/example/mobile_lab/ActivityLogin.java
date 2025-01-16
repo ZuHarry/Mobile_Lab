d7dfc151946093b22c68e8ccd06ea3f780fa4941
MUHAMMAD ZUHAIRY BIN RAZALY <zuhairy812@gmail.com>
2025-01-16 20:22:21 +0800

    Done Lab 3

--- app/src/main/java/com/example/mobile_lab/ActivityLogin.java
+++ app/src/main/java/com/example/mobile_lab/ActivityLogin.java
@@ -1,6 +1,8 @@
 package com.example.mobile_lab;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,56 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.mobile_lab.databinding.ActivityLoginBinding;
+
+
 public class ActivityLogin extends AppCompatActivity {
 
+    ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonLogin.setOnClickListener(this::fnLogin);
+    }
+
+    public ActivityLogin() {
+        super();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    public void fnLogin(View view){
+        String username = binding.editTextLoginName.getText().toString();
+        String password = binding.editTextLoginPassword.getText().toString();
+        Intent intent = new Intent(this,ActivityRegister.class);
+        intent.putExtra("username", username);
+        intent.putExtra("password", password);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/mobile_lab/ActivityRegister.java
+++ app/src/main/java/com/example/mobile_lab/ActivityRegister.java
@@ -1,24 +1,65 @@
 package com.example.mobile_lab;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.example.mobile_lab.databinding.ActivityRegisterBinding;
 
 public class ActivityRegister extends AppCompatActivity {
 
+    ActivityRegisterBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonRegister.setOnClickListener(this::fnCalculateAge);
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Intent intent = getIntent();
+        binding.editTextUsername.setText(intent.getStringExtra("username"));
+        binding.editTextPassword.setText(intent.getStringExtra("password"));
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void fnCalculateAge(View view) {
+        Integer age;
+
+        try {
+            age = Integer.parseInt(binding.editTextDOB.getText().toString());
+
+            age = 2024 - age;
+
+            binding.editTextDOB.setText("Your age is " + age);
+        } catch (Exception e) {
+            e.printStackTrace();
+            binding.editTextDOB.setText("Please enter year in number");
+        }
+
     }
 }
